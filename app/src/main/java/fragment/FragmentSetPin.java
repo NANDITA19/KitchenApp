32f25d28dd1a25fcfff01eef482492013ff45d83
rutvik <rutvik106@live.com>
2016-10-10 02:05:01 +0530

    merged changes from rakshit and nandita, resolved conflicts.

--- app/src/main/java/fragment/FragmentSetPin.java
+++ app/src/main/java/fragment/FragmentSetPin.java
@@ -1,12 +1,34 @@
 package fragment;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by sunil on 9/18/2016.
  */
 public class FragmentSetPin extends Fragment {
+    Context context;
+
+    public static FragmentSetPin getInstance(Context context){
+
+        FragmentSetPin f=new FragmentSetPin();
+        f.context=context;
+
+        return f;
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
 
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
