2c2e919f7fc3c7ee43e93b41b17d6e975a0f57e6
rutvik <rutvik106@live.com>
2016-10-10 00:00:08 +0530

    committing changes pulled from rakshit

--- app/src/main/java/io/fusionbit/kitchenapp/ActivityInitial.java
+++ app/src/main/java/io/fusionbit/kitchenapp/ActivityInitial.java
@@ -1,39 +1,82 @@
 package io.fusionbit.kitchenapp;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.google.firebase.database.DatabaseError;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import model.MenuItem;
-import model.Order;
+import fragment.FragmentMenu;
+import fragment.FragmentOrder;
+import fragment.FragmentStaff;
+import fragment.FragmentTable;
 
-public class ActivityInitial extends KitchenAppActivity
-{
+public class ActivityInitial extends KitchenAppActivity {
 
-    private static final String TAG = AppUtils.APP_TAG + ActivityInitial.class.getSimpleName();
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
 
-        new Handler().postDelayed(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                startActivity(new Intent(ActivityInitial.this,TestActivityViewOrder.class));
-            }
-        },2000);
+        initComponent();
+    }
 
+    private void initComponent() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FragmentOrder(), "Order");
+        adapter.addFragment(new FragmentTable(), "Table");
+        adapter.addFragment(new FragmentMenu(), "Menu");
+        adapter.addFragment(new FragmentStaff(), "Staff");
+
+    }
+
+    public class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 
 
